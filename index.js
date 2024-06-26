b6511653ac7311ecd413ff61ea0df498594e776f
dlacrzz <122063410+dlacrzz@users.noreply.github.com>
2024-06-25 22:44:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
